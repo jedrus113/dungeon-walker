30350e25f048bd8b16840df018c394a72356bd06
Andrew Dabski <jedrus1133@gmail.com>
2019-03-25 21:13:47 +0100

    correct render walls and floor

--- js/game.js
+++ js/game.js
@@ -1,31 +1,47 @@
-var config = {
+import {map1} from "./logic/maps.js";
+
+map1.forEach(level => {
+    for (let i2=0; i2 < level.length; i2++){
+        let row = level[i2];
+        let new_row = [];
+        for (let i=0; i<row.length; i++){
+            new_row.push(row.charAt(i));
+        }
+        level[i2] = new_row;
+    }
+});
+
+const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  physics: {
-    default: 'arcade',
-  },
   scene: {
     preload: preload,
     create: create
   }
 };
 
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
 function preload ()
 {
   this.load.spritesheet('human', 'assets/characters/human.png', { frameWidth: 16, frameHeight: 16 });
-  this.load.spritesheet('wall', 'assets/map/map.png', { frameWidth: 16, frameHeight: 16 });
+  this.load.spritesheet('map', 'assets/map/map.png', { frameWidth: 24, frameHeight: 24 });
 }
 
 function create () {
-  for (let i=0; i<100; i++)
-    this.add.sprite(20*i, 100, 'wall').setFrame(i+300);
+    map1.forEach(level => {
+        for (let i2=0; i2 < level.length; i2++){
+            let row = level[i2];
+            for (let i=0; i<row.length; i++){
+                let el = 3;
+                if (row[i] == '#') el = 0;
+                else if (row[i] == '.') el = 35;
+                this.add.sprite(12 + 24*i, 12 + 24*i2, 'map').setFrame(el);
+            }
+        }
+    });
 
-  this.add.sprite(100, 200, 'wall').setFrame(300);
-
-  var logo = this.add.sprite(400, 200, 'human');
-  logo.setFrame(0);
+  const logo = this.add.sprite(400, 200, 'human').setScale(1.5);
 
 }
